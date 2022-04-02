bdbff38431e50ca05df273449c5027ef269f660d
BuildTools <unconfigured@null.spigotmc.org>
2022-04-02 00:53:20 -0300

    Atualizações no quiz

--- quiz/main.py
+++ quiz/main.py
@@ -1,31 +1,54 @@
 import threading
+import time
 from socket import socket, AF_INET, SOCK_DGRAM
 from _thread import *
 
 def servidor(servidor_socket, jogadores_conectados, limite_usuarios):
+    servidor_lotado = False
     while True:
         if len(jogadores_conectados) >= limite_usuarios:
-            servidor_socket.setblocking(False)
+            servidor_lotado = True
 
-        if servidor_socket.getblocking():
+        try:
             mensagem, endereco = servidor_socket.recvfrom(1024)
-            jogadores_conectados.append(endereco)
-            print(f'[Info] {endereco[0]}:{endereco[1]} entrou na competição')
-            print(f'[Info] {len(jogadores_conectados)} usuários conectados')
+        except:
+            print("[Info] Um usuário se desconectou")
+            continue
+        endereco_str = f'{endereco[0]}:{endereco[1]}'
+        mensagem_cliente = str(mensagem.decode())
 
-            msg = str(mensagem.decode())
-
-            if msg == 'desligar':
+        if jogadores_conectados.get(endereco_str):
+            if mensagem_cliente == 'desligar':
                 print('[Info] Encerrando servidor.')
                 break
+            elif mensagem_cliente == '[Info] Ativo':
+                jogadores_conectados[endereco_str]['status'] = True
+                print(f'[Info] {endereco_str} atualizado com o status: {jogadores_conectados[endereco_str]["status"]}')
 
-            print('')
-            print(f'\n[Info] {endereco[0]}:{endereco[1]}: "{msg}"')
+            print(f'\n[{endereco[0]}:{endereco[1]}]: "{mensagem_cliente}"')
 
-            resp = 'Está na competição'
-            servidor_socket.sendto(resp.encode(), endereco)
         else:
-            checkar_jogadores(jogadores_conectados, servidor_socket)
+            if not servidor_lotado:
+                if mensagem_cliente == 'desligar':
+                    print('[Info] Encerrando servidor.')
+                    break
+
+                jogadores_conectados[endereco_str] = {"ip": endereco, "status": False}
+                print(f'[Info] {endereco[0]}:{endereco[1]} entrou na competição')
+                print(f'[Info] {len(jogadores_conectados)} usuários conectados')
+                print('')
+                print(f'\n[{endereco[0]}:{endereco[1]}] - Nome do jogador: "{mensagem_cliente}"')
+
+                resposta_servidor = '[Info] Bem vindo á competição'
+                servidor_socket.sendto(resposta_servidor.encode(), endereco)
+            else:
+                info_jogador = {
+                    "endereco_str": endereco_str,
+                    "endereco": endereco,
+                    "mensagem_cliente": mensagem_cliente
+                }
+                verificar_jogadores_conectados = threading.Thread(target=checkar_jogadores, args=[jogadores_conectados, servidor_socket, servidor_lotado, info_jogador])
+                verificar_jogadores_conectados.start()
 
 
 def iniciar():
@@ -40,10 +63,50 @@ def desligar():
     print('')
     print('[Info] Servidor sendo desligado')
 
-def checkar_jogadores(jogadores_conectados, servidor_socket):
-    mensagem_jogador = '[Info] Verificando conexão com o jogador..'
-    for ip_jogador in jogadores_conectados:
-        print(servidor_socket.connect(ip_jogador))
+def status(jogadores_conectados, limite_usuarios):
+    print('')
+    print(f'[Info] {len(jogadores_conectados)}/{limite_usuarios} de jogadores conectados')
+
+def checkar_jogadores(jogadores_conectados, servidor_socket, servidor_lotado, info_jogador):
+    endereco_str = info_jogador['endereco_str']
+    endereco = info_jogador['endereco']
+    mensagem_cliente = info_jogador['mensagem_cliente']
+
+    mensagem_jogador = '[Info] Jogador conectado?'
+    print(jogadores_conectados)
+
+    for ip, jogador in jogadores_conectados.items():
+        servidor_socket.sendto(mensagem_jogador.encode(), jogador['ip'])
+
+    time.sleep(5)
+
+    jogadores_desconectados = []
+    for ip, jogador in jogadores_conectados.items():
+        if not jogador["status"]:
+            jogadores_desconectados.append(ip)
+
+    if jogadores_desconectados:
+        for ip in jogadores_desconectados:
+            jogadores_conectados.pop(ip)
+        servidor_lotado = False
+
+    print(jogadores_conectados)
+
+    if not servidor_lotado:
+        jogadores_conectados[endereco_str] = {"ip": endereco, "status": False}
+
+        print(f'[Info] {endereco[0]}:{endereco[1]} entrou na competição')
+        print(f'[Info] {len(jogadores_conectados)} usuários conectados')
+        print('')
+        print(f'\n[{endereco[0]}:{endereco[1]}] - Nome do jogador: "{mensagem_cliente}"')
+    else:
+        resposta_servidor = '[Info] Limite de jogadores alcançado!'
+        servidor_socket.sendto(resposta_servidor.encode(), endereco)
+
+    for ip, jogador in jogadores_conectados.items():
+        jogador["status"] = False
+
+    return servidor_lotado
 
 
 
@@ -55,7 +118,7 @@ def listar_comandos():
     print(f"/desligar (desliga o servidor)")
     print(f"==============================================")
 
-def executar_comandos():
+def executar_comandos(jogadores_conectados, limite_usuarios):
     print('[Info] Caso queira saber a lista de comandos, digite: /comandos')
     while True:
         print('')
@@ -66,6 +129,8 @@ def executar_comandos():
             desligar()
         elif comando == '/finalizar':
             finalizar()
+        elif comando == '/status':
+            status(jogadores_conectados, limite_usuarios)
         elif comando == '/comandos':
             listar_comandos()
         else:
@@ -76,7 +141,7 @@ def main():
     ip = '127.0.0.1'
     porta = 8000
     limite_usuarios = 2
-    jogadores_conectados = []
+    jogadores_conectados = {}
 
     servidor_socket = socket(AF_INET, SOCK_DGRAM)
     servidor_socket.bind((ip, porta))
@@ -90,7 +155,7 @@ def main():
     iniciar_servidor = threading.Thread(target=servidor, args=(servidor_socket, jogadores_conectados, limite_usuarios))
     iniciar_servidor.start()
 
-    executar_comando = threading.Thread(target=executar_comandos, args=[])
+    executar_comando = threading.Thread(target=executar_comandos, args=[jogadores_conectados, limite_usuarios])
     executar_comando.start()
 
 
